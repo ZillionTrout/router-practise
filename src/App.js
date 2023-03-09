ea1c454bd00664dbc76e266f266a631fe9121e85
Estefania Egea <estefania.egcal@gmail.com>
2023-03-09 20:45:50 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './views/Home';
+import Projects from './views/Projects'
+import Contact from './views/Contact'
+import ErrorPage from './views/ErrorPage'
+import Navbar from './components/Navbar';
+import ProjectsGames from './components/ProjectsGames'
+import ProjectsApps from './components/ProjectsApps'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+    <Navbar />
+    <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="projects" element={<Projects/>}>
+        <Route path="/Games" element={<ProjectsGames />} />
+        <Route path="/Apps" element={<ProjectsApps />} />
+      </Route>
+      <Route path="contact" element={<Contact/>} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
     </div>
   );
 }
